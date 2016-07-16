af77c505d8109ef6d479ad2b7696c148b29213b6
SungjungKim <ajax0615@gmail.com>
2016-07-16 21:57:04 +0900

    tesT

---
+++ src/config/env.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _ = require('underscore');
+
+var config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8000;
+}
+
+var environment = {
+    development:
+}
